c121612b409f36feeaae7f5d7ea37f3fe7233a13
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-12 03:04:15 +0600

    Python

---
+++ Python Codes/6.3_For_Loops.py
@@ -0,0 +1,32 @@
+# Print Number from 1 to 10
+
+n = 0
+
+for n in range(1, 11):
+    print(n)
+    n += 1
+
+print()
+
+
+# Reverse Number from 10 to 1
+
+m = 10
+
+for m in range(10, 0, -1):
+    print(m)
+    m -= 1
+
+print()
+
+
+# Print the table of number from user
+
+num = int(input("Enter Number: "))
+i = 0
+
+for i in range(1, 11):
+    print(num * i)
+    i += 1
+
+print()
