1fccc56593e20f749a0270e4ac19d6489db53fa4
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-12 02:59:52 +0600

    Python

---
+++ Python Codes/5.4_Practice.py
@@ -0,0 +1,37 @@
+# Store meaning in dictionary
+
+dic = {
+    "mango" : "fruit",
+    "cat" : "animal",
+    "man" : ["human", "homosapiens"],
+}
+
+print(dic)
+
+# Question 2
+
+subject = {"python", "java", "c++", "c", "python", "java", "c++", "c#"}
+
+print(len(subject))
+
+# Question 3
+
+phy = int(input("Marks: "))
+chem = int(input("Marks: "))
+bio = int(input("Marks: "))
+
+result = {}
+result["Physics"] = phy
+result["Chemistry"] = chem
+result["Biology"] = bio
+
+print(result)
+
+# Question 4
+
+values = {
+    ("int", 9),
+    ("float", 9.0)
+}
+
+print(values)
