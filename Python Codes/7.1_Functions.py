6bd26a8d945549f4e47b300b9bd09d19db527fbb
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-18 03:07:19 +0600

    Python

---
+++ Python Codes/7.1_Functions.py
@@ -0,0 +1,19 @@
+# Basic Function
+def sum(a, b):      # def = Define a function. a & b are parameters
+    ans = a + b
+    return ans
+
+sum1 = sum(5, 10)   # 5 & 10 are arguments
+print(sum1)
+
+print()
+
+
+# Avg using functions
+
+def avg(a, b, c):
+    res = (a + b + c) / 3
+    return res
+
+avg1 = avg(10, 20, 30)
+print(avg1)
