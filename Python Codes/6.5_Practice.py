acec307da673a5fc4b5cc7a67f69765c7e7436f9
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-18 03:06:42 +0600

    Python

---
+++ Python Codes/6.5_Practice.py
@@ -0,0 +1,42 @@
+# Using While Loop
+
+n = 0
+sum1 = 0
+
+while n <= 5:
+    sum1 += n
+    n += 1
+
+print("Using While Loop: ", sum1)
+
+# Using For Loop
+
+m = 0
+sum2 = 0
+
+for m in range(6):
+    sum2 += m
+    m += 1
+
+print("Using For Loop: ", sum2)
+
+# Factorial of 5
+
+i = 1
+fact = 1
+
+for i in range(1, 6):
+    fact *= i
+
+print("Factorial of 5: ", fact)
+
+# Using While Loop
+
+j = 1
+f = 1
+
+while j <= 5:
+    f *= j
+    j += 1
+
+print("Using While Loop: ", f)
