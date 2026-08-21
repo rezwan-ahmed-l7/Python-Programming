27d2172e5ebbdaecdce9d03ac76d85ae5d276a27
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-22 02:15:41 +0600

    Pyhton

---
+++ Python Codes/9.3 Delete File/9.3_Delete_File.py
@@ -0,0 +1,16 @@
+'''Running this code will delete the file xxx.txt file from this folder 
+    and will raise an error if the file is not present, Same for other files in this folder'''
+
+import os
+
+os.remove("xxx.txt")
+
+# Example 2
+
+'''
+
+import os
+
+os.remove("jjj.txt")
+
+'''
